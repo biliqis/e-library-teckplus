fb32d2d2ef71a015d2ff8c60cb660a870dec8d4a
biliqis <biliqistemmy@gmail.com>
2021-11-04 12:53:24 -0400

    final update

--- modules/Books/bookRoutes.js
+++ modules/Books/bookRoutes.js
@@ -1,9 +1,3 @@
-// const router = require("express").Router();
-// const {CreateBook,updateBook } = require('./bookContoller')
-
-// router.post('/books/add-book',CreateBook)
-// router.put('/books/update-books/:id',updateBook)
-// module.exports = router
 
 const router = require("express").Router()
 const { useGuard } = require("../../middleware/guard");
@@ -11,11 +5,11 @@ const { requireAuth } = require("../../middleware/auth.guard");
 const { createBook, getAllBooks, deleteSingleBook, getSingleBook, updateBook } = require("./bookContoller")
 const { createBookGuard, deleteBookGuard, updateBookGuard, checkIfUserIsAdmin } = require("./bookGuard");
 
-router.post("/create", requireAuth, useGuard(createBookGuard), createBook);
+router.post("/create", useGuard(createBookGuard), createBook);
 router.patch("/update/:id", useGuard(updateBookGuard), updateBook);
 
-router.get("/get-books",  getAllBooks);
-router.get("/get-books/:id",  getSingleBook);
+router.get("/get-books", getAllBooks);
+router.get("/get-books/:id", getSingleBook);
 
 router.delete("/delete/:id", useGuard, checkIfUserIsAdmin, useGuard(deleteBookGuard), deleteSingleBook);
 
@@ -26,41 +20,3 @@ module.exports = router;
 
 
 
-
-// const router = require("express").Router();
-// const {
-// 	userSignUp,
-// 	adminSignUp,
-// 	userLogin,
-// 	adminLogin,
-// 	updateUser,
-// 	deleteUser,
-// 	test,
-// 	UserController,
-// } = require("./userController");
-// const { requireAuth } = require("../../middleware/auth.guard");
-// const {
-// 	signUpValidator,
-// 	loginValidator,
-// 	AdminloginValidator,
-// } = require("../../middleware/validator");
-
-// const userValidation = require("./userValidation");
-// const { useGuard } = require("../../middleware/guard");
-// const { userSignUpGuard, userEmailExists, userloginGuard, bodyValidate, userIdExists } = require("./userGuard");
-
-// router.post(
-// 	"/user/sign-up",
-// 	userValidation.SignUp(),
-// 	useGuard(bodyValidate),
-// 	useGuard(userEmailExists),
-// 	userSignUp
-// );
-// router.post("/user/login", userValidation.login(), useGuard(userloginGuard), userLogin);
-// //router.get("/user/test", userValidation.login(), test);
-// //router.post("/admin/sign-up", signUpValidator, adminSignUp);
-// //router.post("/admin/login", AdminloginValidator, adminLogin);
-// router.put("/update-user/:id", requireAuth, useGuard(userIdExists), updateUser);
-// router.delete("/delete-user/:id", requireAuth, useGuard(userIdExists),
-// 	deleteUser);
-// module.exports = router;
