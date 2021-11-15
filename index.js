7e800b2b1dc7a96692a85087c68960a0e3bbfa64
biliqis <biliqistemmy@gmail.com>
2021-11-15 09:41:14 +0100

    borrowing update

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(morgan("dev"))
 app.use(routes);
 const port = process.env.PORT || 8080;
-// app.use((globalError));
 async function bootstrap() {
 	try {
 		await dbConnect();
