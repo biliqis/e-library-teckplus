d42b4ad49ab41ffd577211183dc80f6eb891db3b
biliqis <biliqistemmy@gmail.com>
2021-12-05 13:41:14 +0100

    latest-changes

--- modules/Books/bookRoutes.js
+++ modules/Books/bookRoutes.js
@@ -16,7 +16,7 @@ const checkIfUserIsAdmin = require('../../middleware/auth.guard');
 const bookController = require("../Books/bookContoller");
 
 
-router.patch('/update-return-books', requireAuth, useGuard(bookGuard.checkIfUserIsAdmin),bookContoller.bookReturned)
+router.patch('/update-return-books/:id', requireAuth, useGuard(bookGuard.checkIfUserIsAdmin),bookContoller.bookReturned)
 router.post("/create", requireAuth, upload.single('bookCover'), useGuard(bookGuard.checkIfUserIsAdmin), useBodyValidator(booksValidator.createBookValidator), useGuard(bookGuard.createBookGuard), bookContoller.createBook);
 router.patch("/update/:id", requireAuth, useGuard(bookGuard.checkIfUserIsAdmin), useBodyValidator(booksValidator.editUserValidator), useGuard(bookGuard.updateBookGuard), bookController.updateBook);
 router.get("/get-books", bookController.getAllBooksPagination);
