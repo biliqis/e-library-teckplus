bd69d41fddffadd4492d028ee91f2f6a74142d05
biliqis <biliqistemmy@gmail.com>
2021-12-06 10:32:14 +0100

    latest changes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const userRoutes = require("../../modules/user/userRoutes");
 const bookRoutes = require('../../modules/Books/bookRoutes')
-const booksBorrowing = require("../../modules/booksBorrowing/borrowingRoutes")
+const booksBorrowing=require("../../modules/booksBorrowing/borrowingRoutes")
 const adminRoutes = require("../../modules/admin/adminRoutes")
 
 const viewsRoutes = require("../../modules/views");
